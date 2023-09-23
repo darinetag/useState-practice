d0ff32ab0137829be641db1bad2a21b81f88cae0
Darine Tag <tagdarinefatma@gmail.com>
2023-09-23 19:17:40 +0100

    first commit

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+
+import { useState } from "react";
+const Home = () => {
+  // let name = 'Ella';
+  const [name, setName] = useState ("Ella");
+  const [age, setAge] = useState (200)
+
+  const handleClick = () => {
+    setName("Enchanted Ella");
+    setAge(1000);
+  }
+
+  return (
+    <div className="home">
+      <h2 >Far Faraway Land</h2>
+      <h3>{ name } is { age } years old</h3>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+}
+ 
+export default Home;
+// useState is a hook; It returns an array with two values: 
+// the current state and a function to update it. 
+// The Hook takes an initial state value as an argument and returns an updated state value whenever the setter function is called.
